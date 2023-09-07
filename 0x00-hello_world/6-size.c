64c0ad52d3967d9d47e21091136cd73dd8f8c331
Majesty <majestychibuikem@gmail.com>
2023-09-07 04:54:08 -0700

    printout size of vars on console

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char acharvar;
+	int anintvar;
+	long alongvar;
+	long long alonglongvar;
+	float afloatvar;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharvar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(anintvar));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(alongvar));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglongvar));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
+	return (0);
