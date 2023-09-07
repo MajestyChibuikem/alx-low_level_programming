744db6dca0efc8a185566c4c129ed16837fd0508
Majesty <majestychibuikem@gmail.com>
2023-09-07 04:56:01 -0700

    fix the indentation and missing semicolon

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,7 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(anintvar));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(alongvar));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglongvar));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloatvar));
 
 	return (0);
+}
