d92c8f65639d5576c7704c73e05f6ff32072ab17
Majesty <majestychibuikem@gmail.com>
2023-09-11 05:29:56 -0700

    print all possible num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var <= 9; var++)
+	{
+		putchar(var + '0');
+		if (var < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
