e7b2ad8e925cd6ec0af54d168a8c60f7b1a9741a
Majesty <majestychibuikem@gmail.com>
2023-09-11 03:51:51 -0700

    print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int var;
+
+	for (var = 1; var <= 9; var++)
+	{
+		printf("%d", var);
+	}
+	putchar('\n');
+	return (0);
+}
