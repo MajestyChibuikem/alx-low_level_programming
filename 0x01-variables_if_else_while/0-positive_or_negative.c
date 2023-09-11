351040d20c73f2db818e57d113dec8f29b5b4526
Majesty <majestychibuikem@gmail.com>
2023-09-11 01:06:11 -0700

    fixgit add .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 	{
-		printf("%i is zero\n");
+		printf("%i is zero\n", n);
 	}
 	else if (n > 0)
 	{
-		printf("%i is positive\n");
+		printf("%i is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%i is negative\n");
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }
