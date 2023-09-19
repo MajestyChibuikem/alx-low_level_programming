aadf3f20d2c894e68dd545f5320638a59290edc9
Majesty <majestychibuikem@gmail.com>
2023-09-18 18:31:44 -0700

    outputs a string with a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _puts - prints a string
+ * @str: value to output
+ * Return: str
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
