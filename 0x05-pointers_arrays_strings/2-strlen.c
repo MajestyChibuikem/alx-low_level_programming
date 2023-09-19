74d8895d930b4d570d0cb773cb99bbe850cc8d15
Majesty <majestychibuikem@gmail.com>
2023-09-18 18:25:13 -0700

    lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - outputs length of a char var
+ * @s: value
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len = o;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
