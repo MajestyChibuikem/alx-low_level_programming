35b83be32a71f612873d674d4e5bcc4411314d24
majesty chibuikem <majestychibuikem@gmail.com>
2023-10-26 16:19:01 +0100

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,29 +17,14 @@ void unsigned int binary_to_uint(const char *b)
 
     while(*b)
     {
-        if(*b !='0' && *b != '1')
+        if (*b !='0' && *b != '1')
         {
             return 0;
         }
         num <<= 1;
-        if(*b == '1'){
+        if (*b == '1'){
             num += 1;
         }
-        b++;
-    }
-    return num;
-}
-
-unsigned int binary_to_uint(const char *b) {
-    unsigned int num = 0;
-    if (!b)
-        return 0;
-    while (*b) {
-        if (*b != '0' && *b != '1')
-            return 0;
-        num <<= 1;
-        if (*b == '1')
-            num += 1;
         b++;
     }
     return num;
